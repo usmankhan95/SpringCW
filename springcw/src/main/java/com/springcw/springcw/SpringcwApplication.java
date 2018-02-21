7d1658dfb4fa7a0478e91590e8c1750f1e040c46
Usman Khan <usman_khan95@hotmail.co.uk>
2018-02-21 23:08:54 +0000

    Add files via upload

---
+++ springcw/src/main/java/com/springcw/springcw/SpringcwApplication.java
@@ -0,0 +1,12 @@
+package com.springcw.springcw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcwApplication.class, args);
+	}
+}
